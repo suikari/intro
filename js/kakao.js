6522200e4d2323623ab7ac71c7579ff1faa85745
suikari <suikari@naver.com>
2025-02-06 23:43:12 +0900

    4324

--- js/kakao.js
+++ js/kakao.js
@@ -2,7 +2,7 @@
 window.onload = () => {
     var container = document.getElementById('map');
     var options = { 
-        center: new kakao.maps.LatLng(33.450701, 126.570667), 
+        center: new kakao.maps.LatLng(37.490920171395985, 126.72078754716765), 
         level: 3 
         // disableClickZoom: true //
     };
@@ -11,24 +11,25 @@ window.onload = () => {
     var markerPosition  = new kakao.maps.LatLng(37.490920171395985, 126.72078754716765); 
 
     var marker = new kakao.maps.Marker({
-        position: markerPosition
+        position: markerPosition,
+        text: '더조은 컴퓨터 학원' 
     });
 
     marker.setMap(map);
 
     // marker.setMap(null);    
 
-    kakao.maps.event.addListener(map, 'click', function(mouseEvent) {        
+    // kakao.maps.event.addListener(map, 'click', function(mouseEvent) {        
         
-        var latlng = mouseEvent.latLng; 
+    //     var latlng = mouseEvent.latLng; 
         
-        marker.setPosition(latlng);
+    //     marker.setPosition(latlng);
         
-        var message = '클릭한 위치의 위도는 ' + latlng.getLat() + ' 이고, ';
-        message += '경도는 ' + latlng.getLng() + ' 입니다';
+    //     var message = '클릭한 위치의 위도는 ' + latlng.getLat() + ' 이고, ';
+    //     message += '경도는 ' + latlng.getLng() + ' 입니다';
 
-        var resultDiv = document.getElementById('clickLatlng'); 
-        resultDiv.innerHTML = message;
+    //     var resultDiv = document.getElementById('clickLatlng'); 
+    //     resultDiv.innerHTML = message;
         
-    });
+    // });
 }
