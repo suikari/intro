2df87bd814864ed1d8d16df2d2f03cd1e59f3bd5
suikari <suikari@naver.com>
2025-02-07 18:24:15 +0900

    34345

--- js/popup.js
+++ js/popup.js
@@ -39,3 +39,13 @@ document.addEventListener('DOMContentLoaded', function () {
 
   ele.addEventListener('mousedown', mouseDownHandler);
 });
+
+
+// document.addEventListener('mousemove', (e) => {
+//   let mouseX = e.pageX; // document의 x좌표값
+//   let mouseY = e.pageY; // document의 y좌표값
+
+//   let cursor = document.querySelector('.cursor');
+//   cursor.style.left = mouseX + 'px';
+//   cursor.style.top = mouseY + 'px';
+// })
