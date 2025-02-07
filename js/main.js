f7828e175df6a1e350d0e133681fd2570ca66af1
suikari <suikari@naver.com>
2025-02-07 16:44:34 +0900

    234234

--- js/main.js
+++ js/main.js
@@ -1,6 +1,31 @@
 
 window.onload = () => {
 
+	for (let i = 0; i < 200; i++) {
+		const snow = document.createElement("div");
+		snow.className = "snow";
+		snow.style.opacity = Math.random(); // 무작위 투명도
+	
+		const startX = Math.random() * 100; // 시작 X 좌표 무작위
+		const endX = startX + (Math.random() * 20 - 10); // 종료 X 좌표 무작위
+		const scale = Math.max(Math.random(), 0.5); // 무작위 크기
+	
+		const keyframe = [
+		  { transform: `translate(${startX}vw, 0) scale(${scale})` },
+		  { transform: `translate(${endX}vw, 100vh) scale(${scale})` },
+		];
+	
+		const option = {
+		  duration: 15000 + Math.random() * 5000, // 무작위 지속 시간
+		  easing: "linear",
+		  iterations: Infinity,
+		  delay: -Math.random() * 20 * 1000, // 무작위 지연 시간
+		};
+	
+		snow.animate(keyframe, option);
+		document.body.appendChild(snow);
+	  }
+	  
     let container = document.getElementById('map');
     let options = { 
         center: new kakao.maps.LatLng(37.490920171395985, 126.72078754716765), 
@@ -46,27 +71,3 @@ window.onload = () => {
 	}
 
 
-	for (let i = 0; i < 200; i++) {
-		const snow = document.createElement("div");
-		snow.className = "snow";
-		snow.style.opacity = Math.random(); // 무작위 투명도
-	
-		const startX = Math.random() * 100; // 시작 X 좌표 무작위
-		const endX = startX + (Math.random() * 20 - 10); // 종료 X 좌표 무작위
-		const scale = Math.max(Math.random(), 0.5); // 무작위 크기
-	
-		const keyframe = [
-		  { transform: `translate(${startX}vw, 0) scale(${scale})` },
-		  { transform: `translate(${endX}vw, 100vh) scale(${scale})` },
-		];
-	
-		const option = {
-		  duration: 15000 + Math.random() * 5000, // 무작위 지속 시간
-		  easing: "linear",
-		  iterations: Infinity,
-		  delay: -Math.random() * 20 * 1000, // 무작위 지연 시간
-		};
-	
-		snow.animate(keyframe, option);
-		document.body.appendChild(snow);
-	  }
