a3fc2b1d5f98e16166258d1b6b9364b2a6033a56
suikari <suikari@naver.com>
2025-02-11 14:49:57 +0900

    234

--- js/main.js
+++ js/main.js
@@ -3,14 +3,14 @@ window.onload = () => {
 
 
 
-	for (let i = 0; i < 200; i++) {
+	for (let i = 0; i < 150; i++) {
 		const snow = document.createElement("div");
 		snow.className = "snow";
-		snow.style.opacity = Math.random(); // 무작위 투명도
+		snow.style.opacity = Math.random(); 
 	
-		const startX = Math.random() * 100; // 시작 X 좌표 무작위
-		const endX = startX + (Math.random() * 20 - 10); // 종료 X 좌표 무작위
-		const scale = Math.max(Math.random(), 0.5); // 무작위 크기
+		const startX = Math.random() * 100;
+		const endX = startX + (Math.random() * 20 - 10);
+		const scale = Math.max(Math.random(), 0.5); 
 	
 		const keyframe = [
 		  { transform: `translate(${startX}vw, 0) scale(${scale})` },
@@ -18,10 +18,10 @@ window.onload = () => {
 		];
 	
 		const option = {
-		  duration: 15000 + Math.random() * 5000, // 무작위 지속 시간
+		  duration: 15000 + Math.random() * 5000, 
 		  easing: "linear",
 		  iterations: Infinity,
-		  delay: -Math.random() * 20 * 1000, // 무작위 지연 시간
+		  delay: -Math.random() * 20 * 1000, 
 		};
 	
 		snow.animate(keyframe, option);
