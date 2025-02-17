62b5fa6433d07aee3196c9ebabe18fa2afc88527
suikari <suikari@naver.com>
2025-02-17 12:46:13 +0900

    123123

--- js/swipe.js
+++ js/swipe.js
@@ -1,19 +1,6 @@
 var mob_startX, mob_startY, mob_endX, mob_endY;
 
 
-$('body').on('touchstart', function(e) {
-    alert("터치 시작");
-    var touch = e.originalEvent.touches[0];
-    mob_startX = touch.pageX;
-    mob_startY = touch.pageY;
-  });
-  
-  $('body').on('touchend', function(e) {
-    alert("터치 끝");
-    var touch = e.originalEvent.changedTouches[0];
-    mob_endX = touch.pageX;
-    mob_endY = touch.pageY;
-
 // body 전체에 대해 터치 이벤트를 설정
 $('body').on('touchstart', function(e) {
   var touch = e.originalEvent.touches[0];
@@ -35,10 +22,14 @@ $('body').on('touchend', function(e) {
 
   // X축으로 200픽셀 이상 이동했을 때만 감지
   if (Math.abs(mob_diffX) > 150 && Math.abs(mob_diffY) < 100) {
-    if (diffX > 0) {
-      alert('오른쪽으로 200픽셀 이상 스와이프');
+    if (mob_diffX > 0) {
+      //alert('오른쪽으로 200픽셀 이상 스와이프');
+      next_pagemove();
     } else {
-      alert('왼쪽으로 200픽셀 이상 스와이프');
+      prev_pagemove();
+      //alert('왼쪽으로 200픽셀 이상 스와이프');
     }
   }
 });
+
+  
