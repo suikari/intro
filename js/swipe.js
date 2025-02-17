9109d00700919b35a50b53992f0e76ad789da999
suikari <suikari@naver.com>
2025-02-17 10:43:25 +0900

    666

--- js/swipe.js
+++ js/swipe.js
@@ -1,51 +1,50 @@
 
-var bStartEvent = false;
-//touchstart 이벤트 발생 여부 플래그
-var bMoveEvent = false;
-//touchmove 이벤트 발생 여부 플래그
+// var bStartEvent = false;
+
+// var bMoveEvent = false;
  
-function init(){
-    document.addEventListener("touchstart", this.onStart.bind(this), false);
-    document.addEventListener("touchmove", this.onMove.bind(this), false);
-    document.addEventListener("touchend", this.onEnd.bind(this), false);
-}
+// function init(){
+//     document.addEventListener("touchstart", this.onStart.bind(this), false);
+//     document.addEventListener("touchmove", this.onMove.bind(this), false);
+//     document.addEventListener("touchend", this.onEnd.bind(this), false);
+// }
 
-function onMove(e) {
-    if(!bStartEvent) {
-        return
-    }
-    console.log('swipe');
+// function onMove(e) {
+//     if(!bStartEvent) {
+//         return
+//     }
+//     console.log('swipe');
 
-    var nX = e.changedTouches[0].pageX;
-    var nY = e.changedTouches[0].pageY;
+//     var nX = e.changedTouches[0].pageX;
+//     var nY = e.changedTouches[0].pageY;
     
-    nMoveType = getMoveType(nX, nY);
+//     nMoveType = getMoveType(nX, nY);
     
-    if(nMoveType === 1) {
-        e.preventDefault();
-        console.log('swipe');
-        alert('swipe');
+//     if(nMoveType === 1) {
+//         e.preventDefault();
+//         console.log('swipe');
+//         alert('swipe');
 
-    }
-}
+//     }
+// }
  
-function getMoveType(x, y) {
+// function getMoveType(x, y) {
 
-    var nMoveType = -1;
-    var nX = Math.abs(htTouchInfo.nX- x);
-    var nY = Math.abs(htTouchInfo.nY - y);
-    var nDis = nX + nY;
+//     var nMoveType = -1;
+//     var nX = Math.abs(htTouchInfo.nX- x);
+//     var nY = Math.abs(htTouchInfo.nY - y);
+//     var nDis = nX + nY;
     
-    if(nDis < 25) { return nMoveType }
+//     if(nDis < 25) { return nMoveType }
     
-    var nHSlope = ((window.innerHeight / 2) / window.innerWidth).toFixed(2) * 1;
+//     var nHSlope = ((window.innerHeight / 2) / window.innerWidth).toFixed(2) * 1;
     
-    var nSlope = parseFloat((nY / nX).toFixed(2), 10);
+//     var nSlope = parseFloat((nY / nX).toFixed(2), 10);
     
-    if(nSlope > nHSlope) {
-        nMoveType = 1;
-    } else {
-        nMoveType = 0;
-    }
-    return nMoveType;
-}
+//     if(nSlope > nHSlope) {
+//         nMoveType = 1;
+//     } else {
+//         nMoveType = 0;
+//     }
+//     return nMoveType;
+// }
