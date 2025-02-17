afaaf55b59b91853851662d4d65305979bc578cf
suikari <suikari@naver.com>
2025-02-17 11:54:14 +0900

    1111111

--- js/swipe.js
+++ js/swipe.js
@@ -1,5 +1,19 @@
 var mob_startX, mob_startY, mob_endX, mob_endY;
 
+
+$('body').on('touchstart', function(e) {
+    alert("터치 시작");
+    var touch = e.originalEvent.touches[0];
+    mob_startX = touch.pageX;
+    mob_startY = touch.pageY;
+  });
+  
+  $('body').on('touchend', function(e) {
+    alert("터치 끝");
+    var touch = e.originalEvent.changedTouches[0];
+    mob_endX = touch.pageX;
+    mob_endY = touch.pageY;
+
 // body 전체에 대해 터치 이벤트를 설정
 $('body').on('touchstart', function(e) {
   var touch = e.originalEvent.touches[0];
